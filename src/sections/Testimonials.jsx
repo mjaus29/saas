103a2b799e1429cd6c824fbe65fa0e1adffb3619
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-04-11 19:50:35 +0800

    Fix overlapping issue in Testiomonials on small devices

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -6,7 +6,7 @@ const Testimonials = () => {
 
   return (
     <section className="relative z-2 py-24 md:py-28 lg:py-40">
-      <div className="container block lg:flex">
+      <div className="container flex flex-col lg:flex-row gap-0.5">
         <div className="testimonials_head-res relative z-2 mr-20 flex-300">
           <p className="caption mb-5 max-md:mb-2.5">Wall of Love</p>
           <h3 className="h3 max-md:h-5 text-p4">Words from our fans</h3>
